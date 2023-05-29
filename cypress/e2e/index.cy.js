44e2f96450ad1f1ea234ca8a1e57f5c5b934eb2e
dCampbellPro <davidcampbell4514@gmail.com>
2023-05-28 23:38:12 -0700

    adding cypress tests

---
+++ cypress/e2e/index.cy.js
@@ -0,0 +1,17 @@
+beforeEach(() => {
+    cy.visit('http://localhost:3000/odyssey/');
+})
+it('titles are correct', () => {  
+    cy.get('h1').should('have.text', 'Hello, my name is Jeanine')
+  });
+
+it('navigates to the work page on click', () => {
+    cy.get('a').contains('Work').click();
+    cy.get('h1').should('have.text', 'My Work')
+});
+
+it('correctly colors which page you are at in the top header', () => {
+    cy.get('a').contains('Work').click();
+    cy.get('h1').should('have.text', 'My Work');
+    cy.get('a').contains('Home').should('have.class', 'active')
+});
